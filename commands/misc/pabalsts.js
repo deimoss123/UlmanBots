f4545907681cbf060130dc284f8e64d7b87e515a
deimoss123 <danielsalksnis@gmail.com>
2021-11-08 00:23:05 +0200

    salabots martinsona pabalsts

--- commands/misc/pabalsts.js
+++ commands/misc/pabalsts.js
@@ -8,7 +8,7 @@ export default {
   commands: ['pabalsts'],
   cooldown: 43200000,
   callback: async message => {
-    const guildId = message.guildId
+    const { guildId } = message
     const userId = message.author.id
 
     const martinsonsId = '517429149543956480'
@@ -52,37 +52,49 @@ export default {
     }
 
     if (userId === martinsonsId) {
-
       const rand = Math.floor(Math.random() * 100000)
-      const buttons = [{
-        label: 'Izmantot',
-        style: 1,
-        custom_id: `izman ${rand}`
-      }]
+      const buttons = [
+        {
+          type: 1,
+          components: [
+            {
+              type: 2,
+              label: 'Izmantot',
+              style: 1,
+              custom_id: `izman ${rand}`,
+            },
+          ],
+        },
+      ]
 
       let count = 3
 
-      await buttonEmbed(message, 'Pabalsts', 'Tu saņēmi martinsona pabalstu - 3 latloto biļetes', null, buttons, async i => {
-        if (i.customId === `izman ${rand}`) {
-          await izmantot.callback(message, ['1'], null, null, i, 'latloto')
-          count--
-          if (count <= 0) return { id: `izman ${rand}` }
-        }
-      })
+      await buttonEmbed(
+        message, 'Pabalsts', 'Tu saņēmi martinsona pabalstu - 3 latloto biļetes', null, buttons,
+        async i => {
+          if (i.customId === `izman ${rand}`) {
+            await izmantot.callback(message, ['1'], null, null, i, 'latloto')
+            count--
+            if (count <= 0) return { id: `izman ${rand}` }
+          }
+        })
       await addItems(guildId, userId, { latloto: 3 })
       return 1
-    } else {
-      for (const role in roles) {
-        if (message.member.roles.cache.some(r => r.id === roles[role].id)) {
-          console.log(role, 'role')
-          message.reply(embedTemplate(
-            message, 'Pabalsts', `Tu saņēmi ${roles[role].name} pabalstu **${roles[role].lati}** latu apmērā`))
-          await addLati(guildId, userId, roles[role].lati)
-          return 1
-        }
-      }
-      message.reply(embedError(message, 'Pabalsts', 'Lai saņemtu pabalstu tev vajag vismaz diždraudziņdauņa lomu'))
-      return 2
     }
+
+    for (const role in roles) {
+      if (message.member.roles.cache.some(r => r.id === roles[role].id)) {
+        console.log(role, 'role')
+        message.reply(embedTemplate(
+          message, 'Pabalsts',
+          `Tu saņēmi ${roles[role].name} pabalstu **${roles[role].lati}** latu apmērā`))
+        await addLati(guildId, userId, roles[role].lati)
+        return 1
+      }
+    }
+    message.reply(embedError(message, 'Pabalsts',
+      'Lai saņemtu pabalstu tev vajag vismaz diždraudziņdauņa lomu'))
+    return 2
+
   },
 }
